03a4380516d8ffd4a5b0a3a3fee2da7480e34b10
mongarerobert3 <mongarerobert3@gmail.com>
2025-02-28 14:49:38 +0300

    Another port

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -11,7 +11,7 @@ interface SimilarImage {
 const VisionSearch = () => {
   const [queryImage, setQueryImage] = useState<File | null>(null);
   const [similarImages, setSimilarImages] = useState<SimilarImage[]>([]);
-  const BACKEND_URL = process.env.REACT_APP_BACKEND_URL ;
+  const BACKEND_URL = "https://vision-search.onrender.com";
 
   const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
